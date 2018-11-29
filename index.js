3680a9a6eb2818343b80aa851f701dde7bdcd7f4
Dominic Tremblay <dominictremblay24@gmail.com>
2018-11-29 13:14:01 -0500

    express initial setup

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+const app = express();
+const port = 8080;
+
+const movieQuotesDb = {
+  'd9424e04-9df6-4b76-86cc-9069ca8ee4bb': {
+    id: 'd9424e04 - 9df6-4b76-86cc-9069ca8ee4bb',
+    quote: 'Why so serious?',
+  },
+  '27b03e95 - 27d3-4ad1-9781 - f4556c1dee3e': {
+    id: '27b03e95-27d3 - 4ad1 - 9781 - f4556c1dee3e',
+    quote: 'YOU SHALL NOT PASS!',
+  },
+  '5b2cdbcb-7b77-4b23-939f-5096300e1100': {
+    id: '5b2cdbcb-7b77-4b23-939f-5096300e1100',
+    quote: "It's called a hustle, sweetheart.",
+  },
+  '917d445c-e8ae-4ed9-8609-4bf305de8ba8': {
+    id: '917d445c-e8ae-4ed9-8609-4bf305de8ba8',
+    quote: 'The greatest teacher, failure is.',
+  },
+  '4ad11feb-a76a-42ae-a1c6-8e30dc12c3fe': {
+    id: '4ad11feb-a76a-42ae-a1c6-8e30dc12c3fe',
+    quote: 'Speak Friend and Enter',
+  },
+};
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.redirect('/quotes');
+});
+
+app.listen(port, () => console.log(`Express server running on port ${port}`));
